01378730fce39b8cef276ae8f8e3d69d73147256
robzelluf <robzelluf@hotmail.com>
2019-11-19 14:48:42 +0200

    Try some training of LSTM

--- Model.py
+++ Model.py
@@ -11,7 +11,10 @@ class Model(nn.Module):
         self.num_speakers = num_speakers
 
         self.lstm1 = nn.LSTM(input_dim, 256, 3, batch_first=True)
+        self.linear1 = nn.Linear(256, num_speakers)
 
     def forward(self, x):
-        x = self.lstm1(x)
-        return x
+        lstm_out, hidden = self.lstm1(x)
+        logits = self.linear1(lstm_out[-1])
+        speaker_scores = F.log_softmax(logits, dim=1)
+        return speaker_scores
--- train_model.py
+++ train_model.py
@@ -3,7 +3,17 @@ from utils import read_data
 from Model import *
 import torch
 
+
+def get_accuracy(logits, target):
+    """ compute accuracy for training round """
+    corrects = (
+            torch.max(logits, 1)[1].view(target.size()).data == target.data
+    ).sum()
+    accuracy = 100.0 * corrects / len(target)
+    return accuracy.item()
+
 X, Y = read_data()
+num_speakers = np.max(Y) + 1
 
 X = torch.from_numpy(X)
 X = X.flatten(1, -1)
@@ -12,9 +22,15 @@ input_dim = X.shape[1]
 X = X.reshape(1, X.shape[0], X.shape[1])
 Y = torch.from_numpy(Y)
 
-num_speakers = torch.max(Y) + 1
-
 model = Model(input_dim, num_speakers)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-y = model.forward(X)
+iterations = 1000
+for i in range(iterations):
+    y = model.forward(X)
+    loss = torch.nn.NLLLoss()(y, Y)
+    loss.backward()
+    optimizer.step()
+    print(loss.detach())
+    print(get_accuracy(y, Y))
 
