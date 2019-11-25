95a665c827356fd09a4ca86eba5e908484b5fc01
robzelluf <robzelluf@hotmail.com>
2019-11-25 22:52:35 +0200

    Upload everything

--- Model.py
+++ Model.py
@@ -7,25 +7,21 @@ import torch.nn.functional as F
 class Model(nn.Module):
     def __init__(self, input_dim, num_speakers):
         super(Model, self).__init__()
-        self.input_dim = input_dim
         self.num_speakers = num_speakers
 
-        self.lstm1 = nn.LSTM(input_dim, 256, 3, batch_first=True)
-        self.linear1 = nn.Linear(1536, 512)
-        self.linear2 = nn.Linear(512, 512)
-        self.linear3 = nn.Linear(512, 700)
-        self.linear4 = nn.Linear(700, num_speakers)
+        self.conv1 = nn.Conv2d(1, 8, 4, 2)
+        self.conv2 = nn.Conv2d(8, 16, 4, 1)
+        self.linear1 = nn.Linear(16 * 11 * 96, 700)
+        self.linear2 = nn.Linear(700, num_speakers)
 
     def forward(self, x):
-        lstm_out, x = self.lstm1(x)
-        x = torch.cat(x).flatten()
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+
+        x = x.view(-1, (16 * 11 * 96))
+
         x = F.relu(self.linear1(x))
         x = F.relu(self.linear2(x))
-
-        x = F.avg_pool1d(x, 5)
-
-        x = F.relu(self.linear3(x))
-        x = F.normalize(x, 2, 1)
         x = F.softmax(x)
         return x
 
--- train_model.py
+++ train_model.py
@@ -14,13 +14,13 @@ def get_accuracy(logits, target):
 
 
 X, Y = read_data()
-num_speakers = len(Y[0])
+m = X.shape[0]
+num_speakers = max(Y + 1)
 
 X = torch.from_numpy(X)
-X = X.flatten(1, -1)
-input_dim = X.shape[1]
+input_dim = [X.shape[1], X.shape[2]]
+X = X.reshape((m, 1, input_dim[0], input_dim[1]))
 
-X = X.reshape(1, X.shape[0], X.shape[1])
 Y = torch.from_numpy(Y)
 
 model = Model(input_dim, num_speakers)
@@ -29,9 +29,10 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 iterations = 1000
 for i in range(iterations):
     y = model.forward(X)
-    loss = torch.nn.NLLLoss()(y, Y)
+
+    loss = torch.nn.CrossEntropyLoss()(y, Y)
     loss.backward()
+    optimizer.zero_grad()
     optimizer.step()
-    print(loss.detach())
-    print(get_accuracy(y, Y))
+    print("Iteration", i, "out of", iterations, ". Loss:", round(float(loss.detach()), 2), "- Accuracy", get_accuracy(y, Y))
 
