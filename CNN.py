0a10d3a16444635da660ce7d4722e1dd0897afb9
robzelluf <robzelluf@hotmail.com>
2019-12-12 11:46:29 +0200

    Make CNN bit smaller

--- CNN.py
+++ CNN.py
@@ -8,11 +8,11 @@ class Model(nn.Module):
     def __init__(self, num_speakers):
         super(Model, self).__init__()
         self.num_speakers = num_speakers
-        self.lin_size = 32 * 13 * 98
+        self.lin_size = 16 * 13 * 98
 
-        self.conv1 = nn.Conv2d(1, 128, 4, 2)
-        self.conv2 = nn.Conv2d(128, 64, 2, 1)
-        self.conv3 = nn.Conv2d(64, 32, 1, 1)
+        self.conv1 = nn.Conv2d(1, 64, 4, 2)
+        self.conv2 = nn.Conv2d(64, 32, 2, 1)
+        self.conv3 = nn.Conv2d(32, 16, 1, 1)
         self.pool = nn.MaxPool2d(1, 1)
         self.linear1 = nn.Linear(self.lin_size, 700)
         self.linear2 = nn.Linear(700, num_speakers)
