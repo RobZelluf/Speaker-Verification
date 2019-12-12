e4cab88394d7f8e00c146f98e398ae25b027b86b
robzelluf <robzelluf@hotmail.com>
2019-12-12 14:38:44 +0200

    Decrease batch size and add hidden layer

--- LSTM.py
+++ LSTM.py
@@ -5,18 +5,18 @@ import torch.nn.functional as F
 
 
 class Model(nn.Module):
-    def __init__(self, input_dim, batch_size, output_dim=8, hidden_dim = 64, num_layers=2):
+    def __init__(self, input_dim, batch_size, output_dim=8, num_layers=2):
         super(Model, self).__init__()
         self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
         self.batch_size = batch_size
         self.num_layers = num_layers
 
         # setup LSTM layer
-        self.lstm = nn.LSTM(self.input_dim[0], self.hidden_dim, self.num_layers)
+        self.lstm = nn.LSTM(self.input_dim[0], 512, self.num_layers)
 
         # setup output layer
-        self.linear = nn.Linear(self.hidden_dim, output_dim)
+        self.linear1 = nn.Linear(512, 265)
+        self.linear2 = nn.Linear(265, output_dim)
 
     def init_hidden(self):
         return (
@@ -26,8 +26,9 @@ class Model(nn.Module):
 
     def forward(self, x):
         lstm_out, hidden = self.lstm(x)
-        logits = self.linear(lstm_out[-1])
-        pred = F.log_softmax(logits, dim=1)
+        x = self.linear1(lstm_out[-1])
+        x = self.linear2(x)
+        pred = F.log_softmax(x, dim=1)
         return pred
 
     def get_accuracy(self, logits, target):
