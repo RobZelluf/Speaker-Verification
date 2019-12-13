f17cc26e897009387d355161f6c88862f22ed72c
robzelluf <robzelluf@hotmail.com>
2019-12-13 14:54:10 +0200

    New embeddings

--- read_CNN_model.py
+++ read_CNN_model.py
@@ -3,12 +3,12 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
-model = torch.load("models/CNN1/CNN.pth")
+model = torch.load("models/new-CNN/CNN.pth")
 
 embeddings = model.linear2.weight.data.numpy()
 embeddings = np.transpose(embeddings)
 
-np.save("embeddings/CNN1.npy", embeddings)
+np.save("embeddings/new-CNN/embeddings.npy", embeddings)
 
 with open("models/CNN1/performance.p", "rb") as f:
     train_accs, test_accs = pickle.load(f)
