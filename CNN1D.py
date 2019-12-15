8e343b18d47242700860822f0e84e445b4cf4669
robzelluf <robzelluf@hotmail.com>
2019-12-15 16:23:08 +0200

    Train 1D CNN

--- CNN1D.py
+++ CNN1D.py
@@ -5,15 +5,19 @@ import torch.nn.functional as F
 
 
 class Model(nn.Module):
-    def __init__(self, num_speakers):
+    def __init__(self, input_dim, num_speakers, embedding_dim=700):
         super(Model, self).__init__()
         self.num_speakers = num_speakers
+        self.input_dim = input_dim
+        self.embedding_dim = embedding_dim
+        self.lin_size = 32 * 98
+        print("Embedding_dim:", embedding_dim)
 
-        self.conv1 = nn.Conv2d(1, 16, 4, 2)
-        self.conv2 = nn.Conv2d(16, 8, 2, 1)
-        self.conv3 = nn.Conv2d(8, 4, 1, 1)
-        self.pool = nn.MaxPool2d(1, 1)
-        self.linear1 = nn.Linear(4 * 13 * 98, 700)
+        self.conv1 = nn.Conv1d(input_dim[0], 128, 4, 2)
+        self.conv2 = nn.Conv1d(128, 64, 2, 1)
+        self.conv3 = nn.Conv1d(64, 32, 1, 1)
+        self.pool = nn.AvgPool2d(1, 1)
+        self.linear1 = nn.Linear(self.lin_size, 700)
         self.linear2 = nn.Linear(700, num_speakers)
 
     def forward(self, x):
@@ -22,8 +26,7 @@ class Model(nn.Module):
         x = F.relu(self.conv3(x))
 
         x = self.pool(x)
-
-        x = x.view(-1, (4 * 13 * 98))
+        x = x.view(-1, self.lin_size)
 
         x = F.relu(self.linear1(x))
         x = F.relu(self.linear2(x))
