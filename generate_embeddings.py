b162f280402b6b2721bc9d20d4cfa9c5cad8fce2
robzelluf <robzelluf@hotmail.com>
2019-12-15 12:33:00 +0200

    Make embeddings script more structured

--- generate_embeddings.py
+++ generate_embeddings.py
@@ -5,22 +5,35 @@ import torch
 from CNN import *
 import os
 
-DIR = input("Save in directory:")
-if not os.path.exists("embeddings/" + DIR):
-    os.mkdir("embeddings/" + DIR)
+DIRs = os.listdir("models/")
+for i in range(len(DIRs)):
+    if DIRs[i][0] == "_":
+        continue
 
+    print(i, DIRs[i])
+
+dir_ind = int(input("Model number:"))
+DIR = DIRs[dir_ind]
+
+# Create folder for embeddings if it does not yet exist
+if not os.path.exists("models/" + DIR + "/embeddings"):
+    os.mkdir("models/" + DIR + "/embeddings")
+
+model = torch.load("models/" + DIR + "/model.pth")
 X, Y = read_data()
+
+# Load training indices and test indices
+with open("models/" + DIR + "/train-test.p", "rb") as f:
+    train_ind, test_ind = pickle.load(f)
+
+print("Train samples:", len(train_ind))
+print("Test_samples:", len(test_ind))
 
 m = X.shape[0]
 input_dim = [X.shape[1], X.shape[2]]
 num_speakers = max(Y + 1)
-X = X.reshape((m, 1, input_dim[0], input_dim[1]))
-
+X = X.reshape((m, input_dim[1], input_dim[0]))
 X = torch.from_numpy(X)
-# Y = torch.from_numpy(Y)
-
-model = torch.load("models/CNN1/CNN.pth")
-
 
 for i in range(num_speakers):
     indices = []
@@ -29,9 +42,8 @@ for i in range(num_speakers):
             indices.append(j)
 
     embeddings = model(X[indices])
-    print(embeddings.shape)
 
-    with open("embeddings/" + DIR + "/" + str(i) + ".p", "wb") as f:
+    with open("models/" + DIR + "/embeddings/" + str(i) + ".p", "wb") as f:
         pickle.dump(embeddings, f)
 
 
